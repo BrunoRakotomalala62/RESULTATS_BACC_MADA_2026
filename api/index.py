958885cf8bb94f29badee4bb4cfb0789971ac99c
BrunoRakotomalala62 <brunorakotomalala859@gmail.com>
2026-07-28 08:28:55 +0300

    feat: point d'entrée WSGI pour Vercel

---
+++ api/index.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Ajouter le répertoire parent au path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from server import app
+
+# Point d'entrée WSGI pour Vercel
